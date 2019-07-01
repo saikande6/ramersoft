84f28208a08f05860fb9d5f267e2b9cacb1d418e
saikande6 <41055603+saikande6@users.noreply.github.com>
2019-07-01 11:42:32 +0530

    done changes

--- ramersoft/src/com/ramersoft/Home1.java
+++ ramersoft/src/com/ramersoft/Home1.java
@@ -1,12 +1,8 @@
 package com.ramersoft;
 
 import java.awt.AWTException;
-import java.awt.Desktop.Action;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.By.ById;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
--- ramersoft/src/com/ramersoft/Homepage.java
+++ ramersoft/src/com/ramersoft/Homepage.java
@@ -1,17 +1,24 @@
 package com.ramersoft;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Homepage {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\ramer\\Downloads\\chromedriver.exe");
-		WebDriver driver =new ChromeDriver();
-        driver.get("https://ramersoft.com");
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\ramer\\Desktop\\selenium documents\\geckodriver.exe");
+		WebDriver driver =new FirefoxDriver();
+        driver.get("http://192.168.1.113:8080/KOT/getLayout");
         driver.manage().window().maximize();
-        driver.close();
+        driver.findElement(By.id("layout_123esdrf45t6yhgb")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"tabler_\"]/span")).click();
+        
+        
+        
 	}
 
 }
